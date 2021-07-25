9c676202f0b714fbaea54bc5f8397de4732c8cad
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-07-25 21:59:56 +0200

    Declarations methods cListTemplate
    Add declarations of all methods in cListTemplate class.

--- Data_structures/Data_structures/clisttemplate.h
+++ Data_structures/Data_structures/clisttemplate.h
@@ -36,7 +36,171 @@ class cListTemplate
 
 /********** PUBLIC: BEGINNING **********/
 public:
+    /*
+     * cListTemplate() - konstruktor wywolywany bez argumentow.
+     * PRE:
+     * - brak;
+     * POST:
+     * - tworzenie obiektu klasy cListTemplate.
+     */
     cListTemplate();
+
+    /*
+     * cListTemplate(typeLoop aSize) - konstruktor wywolywany
+     * z jednym argumentem - liczba elementow jakie mamy
+     * dodac do listy.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop);
+     * POST:
+     * - tworzenie obiektu klasy cListTemplate.
+     */
+    cListTemplate(typeLoop aSize);
+
+    /*
+     * cListTemplate(typeLoop aSize, typeData aDrawingRange) -
+     * konstruktor wywolywany z dwoma argumentami - liczba
+     * elementow jakie mamy dodac do listy oraz zakresem
+     * z jakiego mamy te elementy losowac.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop);
+     * - podanie maksymalnego zakresu losowania elementow
+     * (typ: typeData);
+     * POST:
+     * - tworzenie obiektu klasy cListTemplate.
+     */
+    cListTemplate(typeLoop aSize, typeData aDrawingRange);
+
+
+
+    /*
+     * typeData getFirstElement() - metoda zwracajaca
+     * pierwszy element z listy.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie pierwszego elementu z listy
+     * (typ: typeData).
+     */
+    typeData getFirstElement();
+
+    /*
+     * typeData getLastElement() - metoda zwracajaca
+     * ostatni element z listy.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie ostatniego elementu z listy
+     * (typ: typeData).
+     */
+    typeData getLastElement();
+
+    /*
+     * typeLoop getListSize() - zwrocenie rozmiaru listy.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie rozmiaru listy (typ: typeLoop).
+     */
+    typeLoop getListSize();
+
+    /*
+     * typeLoop getDrawingRange() - metoda zwracajaca
+     * wartosc zakresu losowania elementow.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie wartosci pola vDrawingRange
+     * (typ: typeLoop).
+     */
+    typeLoop getDrawingRange();
+
+    /*
+     * bool getListIsEmpty() - metodo zawracajaca
+     * informacje o tym czy lista jest pusta.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy lista jest pusta
+     * (typ: bool).
+     */
+    bool getListIsEmpty();
+
+
+
+    /*
+     * void mAddElementToBegin(typeData aElement) - metoda
+     * dopisujaca element na sam poczatek listy.
+     * PRE:
+     * - podanie wartosci elementu (typ: typeData);
+     * POST:
+     * - brak.
+     */
+    void mAddElementToBegin(typeData aElement);
+
+    /*
+     * void mAddElementToEnd(typeData aElement) - metoda
+     * dopisujaca element na samym koncu listy.
+     * PRE:
+     * - podanie wartosci elementu (typ: typeData);
+     * POST:
+     * - brak.
+     */
+    void mAddElementToEnd(typeData aElement);
+
+    /*
+     * bool mRemoveElementFromBegin() - metoda usuwajaca
+     * element z poczatku listy (o ile lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy usuniecie elementu
+     * sie udalo (typ: bool).
+     */
+    bool mRemoveElementFromBegin();
+
+    /*
+     * bool mRemoveElementFromEnd() - metoda usuwajaca
+     * element z konca listy (o ile lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy usuniecie elementu
+     * sie udalo (typ: bool).
+     */
+    bool mRemoveElementFromEnd();
+
+
+
+    /*
+     * void mPrintFirstElement() - metoda wyswietlajaca
+     * pierwszy element listy (o ile lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintFirstElement();
+
+    /*
+     * void mPrintLastElement() - metoda wyswietlajaca
+     * ostatni element listy (o ile lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintLastElement();
+
+    /*
+     * void mPrintAllElements() - metoda wyswietlajaca
+     * cala zawartosc listy (o ile lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintAllElements();
+
 
 /********** PUBLIC: END **********/
 };
