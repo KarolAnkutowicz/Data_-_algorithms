12012a4a2382976fa281b0a15e60b851d5d5ec5c
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-09-14 16:29:54 +0200

    Sorting methods cTable
    Add declarations of methods which representing sorting algorithms in cTable class.

--- Sorting_algorithms/Sorting_algorithms/ctable.h
+++ Sorting_algorithms/Sorting_algorithms/ctable.h
@@ -13,8 +13,8 @@
 using namespace std;
 
 /*
- * cTable - klasa modelujaca tablice, wraz
- * z podstawowymi operacjami oraz algorytmami
+ * cTable - klasa modelujaca tablice,
+ * wraz z podstawowymi operacjami oraz algorytmami
  * sortowania jej elementow.
  */
 class cTable
@@ -32,6 +32,77 @@ public:
      * - utworzenie obiektu klasy cTable.
      */
     cTable();
+
+    /*
+     * cTable(int aLength) - konstruktor wywolywany
+     * ze jednym argumentem - liczba elementow tablicy.
+     * PRE:
+     * - podanie liczby elementwo tablicy (typ: int);
+     * POST:
+     * - utworzenie obiektu klasy cTable.
+     */
+    cTable(int aLength);
+
+    /*
+     *
+     */
+    void mPrintTable();
+
+    void mWriteTableToFile();
+
+    void mReadTableFromFile();
+
+
+
+    void mQuicksort();
+
+    void mMergeSort();
+
+    void mInPlaceMergeSort();
+
+    void mIntrosort();
+
+    void mHeapsort();
+
+    void mInsertionSort();
+
+    void mBlockSort();
+
+    void mTimsort();
+
+    void mSelectionSort();
+
+    void mCubesort();
+
+    void mShellsort();
+
+    void mBubbleSort();
+
+    void mExchangeSort();
+
+    void mTreeSort();
+
+    void mCycleSort();
+
+    void mLibrarySort();
+
+    void mPatienceSorting();
+
+    void mSmoothsort();
+
+    void mStrandSort();
+
+    void mTournamentSort();
+
+    void mCocktailShakerSort();
+
+    void mCombSort();
+
+    void mGnomeSort();
+
+    void mOddEvenSort();
+
+
 /********** PUBLIC: END **********/
 };
 
