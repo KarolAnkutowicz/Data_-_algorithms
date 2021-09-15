1a93901b038772c8d2b7299a553b7008ed69c693
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-09-15 23:42:57 +0200

    Constructors cTable
    Add declarations of constructors and destructor in cTable class.

--- Sorting_algorithms/Sorting_algorithms/ctable.h
+++ Sorting_algorithms/Sorting_algorithms/ctable.h
@@ -21,6 +21,28 @@ class cTable
 {
 /********** PRIVATE: BEGIN **********/
 
+    /*
+     * int *tabElements - wskaznik do tablicy elementow
+     * typu int.
+     */
+    int *tabElements;
+
+    /*
+     * int vSeries - liczba serii danych.
+     */
+    int vSeries;
+
+    /*
+     * int vLength - liczba danych w pojedynczej serii.
+     */
+    int vLength;
+
+    /*
+     * int vDrawingRange - maksymalny zakres losowania
+     *elementow tablicy.
+     */
+    int vDrawingRange;
+
 /********** PRIVATE: END **********/
 /********** PUBLIC: BEGIN **********/
 public:
@@ -34,14 +56,50 @@ public:
     cTable();
 
     /*
-     * cTable(int aLength) - konstruktor wywolywany
-     * ze jednym argumentem - liczba elementow tablicy.
+     * cTable(int aSeries) - konstruktor wywolywany
+     * ze jednym argumentem - liczba serii.
      * PRE:
-     * - podanie liczby elementwo tablicy (typ: int);
+     * - podanie liczby serii (typ: int);
      * POST:
      * - utworzenie obiektu klasy cTable.
      */
-    cTable(int aLength);
+    cTable(int aSeries);
+
+    /*
+     * cTable(int aSeries, int aLength) - konstruktor
+     * wywolywany z dwoma argumentami - liczba serii
+     * oraz liczba elementow w pojedynczej serii.
+     * PRE:
+     * - podanie liczby serii (typ: int);
+     * - podanie dlugosci pojedynczej serii (typ: int)
+     * POST:
+     * - utworzenie obiektu klasy cTable.
+     */
+    cTable(int aSeries, int aLength);
+
+    /*
+     * cTable(int aSeries, int aLength, int aDrawingRange) -
+     * konstruktor wywolywany z trzema argumentami -
+     * liczba serii, liczba elementow w pojedynczej serii
+     * oraz zakresem losowania elementow.
+     * PRE:
+     * - podanie liczby serii (typ: int);
+     * - podanie dlugosci pojedynczej serii (typ: int)
+     * - podanie zakresu losowanie elementow (typ: int)
+     * POST:
+     * - utworzenie obiektu klasy cTable.
+     */
+    cTable(int aSeries, int aLength, int aDrawingRange);
+
+    /*
+     * ~cTable() - destruktor klasy cTable.
+     * PRE:
+     * brak;
+     * POST:
+     * - zwalnianie zasobow przydzielanych dynamicznie.
+     */
+    ~cTable();
+
 
     /*
      *
