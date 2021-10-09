de9768b274385bf453ce36335653f2c7341866e8
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-10-10 01:01:59 +0200

    Remove elements cListMy
    Redefinition method which remove elements from end list in cListMy class.

--- Data_structures/Data_structures/clistmy.cpp
+++ Data_structures/Data_structures/clistmy.cpp
@@ -148,35 +148,37 @@ bool cListMy::mRemoveElementFromBegin()
  */
 bool cListMy::mRemoveElementFromEnd()
 {
-    //USUWANIE ELEMENTU Z KONCA
-    // sprawdzamy czy lista jest pusta
-        // jesli tak to nie mamy co usuwac z listy
-    // jesli lista nie jest pusta
-        // jesli jest na niej jeden element
-            // likwidujemy pierwszy element listy
-            // poczatek listy ustanawiamy na NULL
-            // koniec listy ustanawiamy na NULL
-            // ustanawiamy ze lista jest pusta
-        // jesli jest na niej wiecej niz jeden element
-            // tworzymy element pomocniczy
-            // przechodzimy przez wszystkie elementy
-                // przypisujemy pomocniczy element jako pierwszy element
-                // sprawdzamy czy analizowany element wskazuje jako nastepnik na ostatni element
-                    // jesli tak to zmieniamy jego wskazanie na nastepnik jako NULL
-                    // usuwamy aktualnie ostatni element
-                    // ustanawiamy nowy ostatni element jako biezacy
-                // jesli tak nie jest
-                    // wowczas jako element pomocniczy wskazujemy nastepnik biezacego elementu
-            // niszczymy element pomocniczy
-        // zmniejszamy rozmiar listy
-
-    /*if (vIsEmpty == true) //
-        return false; //
-    else //
+    if (vIsEmpty == true) // sprawdzamy czy lista jest pusta
+        return false; // jesli tak to nie mamy co usuwac z listy
+    else // jesli lista nie jest pusta
     {
-        cListMyElement Elem; //
-        Elem = vListMyEnd;
-    }*/
+        if (vSize == 1) // jesli jest na niej jeden element
+        {
+            vListMyBegin->~cListMyElement(); // likwidujemy pierwszy element listy
+            vListMyBegin = NULL; // poczatek listy ustanawiamy na NULL
+            vListMyEnd =  NULL; // koniec listy ustanawiamy na NULL
+            vIsEmpty = true; // ustanawiamy ze lista jest pusta
+        }
+        else // jesli jest na niej wiecej niz jeden element
+        {
+            cListMyElement *Elem; // tworzymy element pomocniczy
+            Elem = vListMyBegin; // przypisujemy pomocniczy element jako pierwszy element
+            for (typeLoop i = 0; i < vSize; i++)// przechodzimy przez wszystkie elementy
+            {
+                if ((Elem->getNext()) == vListMyEnd)// sprawdzamy czy analizowany element wskazuje jako nastepnik na ostatni element
+                {
+                    Elem->setNext(NULL); // jesli tak to zmieniamy jego wskazanie na nastepnik jako NULL
+                    vListMyEnd->~cListMyElement(); // usuwamy aktualnie ostatni element
+                    vListMyEnd = Elem; // ustanawiamy nowy ostatni element jako biezacy
+                }
+                else// jesli tak nie jest
+                    Elem = Elem->getNext(); // wowczas jako element pomocniczy wskazujemy nastepnik biezacego elementu
+            }
+            Elem->~cListMyElement(); // niszczymy element pomocniczy
+        }
+        vSize--; // zmniejszamy rozmiar listy
+        return true; // zwracamy informacje, ze element zostal usuniety
+    }
 }
 
 /*
