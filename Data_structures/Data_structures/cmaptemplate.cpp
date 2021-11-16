6a4b5ad213d087157d697fef5c7040bf4c84e78f
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-11-16 01:46:46 +0100

    Constructors cMapTemplate
    Add definitions of constructors in cMapTemplate class.

--- Data_structures/Data_structures/cmaptemplate.cpp
+++ Data_structures/Data_structures/cmaptemplate.cpp
@@ -8,9 +8,87 @@
 
 using namespace std;
 
+/********** PUBLIC: BEGIN **********/
+
+/*
+ * cMapTemplate()
+ */
 cMapTemplate::cMapTemplate()
 {
+    vDrawingRangeValue = 0; // ustanowienie zakresu losowania elementow
 }
+
+/*
+ * cMapTemplate(typeLoop aSize)
+ */
+cMapTemplate::cMapTemplate(typeLoop aSize)
+{
+    vDrawingRangeValue = 0; // ustanowienie zakresu losowania elementow
+    typeLoop vChar; // utworzenie zmiennej wykorzystywanej do tworzenia kluczy
+    srand(time_t(NULL)); // ustanowienie zmiennej losowej
+    for (typeLoop i = 0; i < aSize; i++) // przejscie po wszystkich elementach
+    {
+        vChar = i + 97; // przesuniecie zmiennej do wartosci w tablicy ASCII do malych liter
+        MapTemplate[(char)vChar] = rand() % vDrawingRangeValue; // dodanie nowego elementu
+    }
+}
+
+/*
+ * cMapTemplate(typeLoop aSize)
+ */
+cMapTemplate::cMapTemplate(typeLoop aSize, typeData aDrawingRangeValue)
+{
+    vDrawingRangeValue = aDrawingRangeValue; // ustanowienie zakresu losowania elementow
+    mDrawElements(aSize); // wywolanie metody losujacej elementy
+}
+
+
+
+/*
+ * typeData getElement(typeKey aKey)
+ */
+typeData cMapTemplate::getElement(typeKey aKey)
+{
+
+}
+
+
+
+/*
+ * void mAddElement(cElementMapAndSet aElement)
+ */
+void cMapTemplate::mAddElement(cElementMapAndSet aElement)
+{
+
+}
+
+/*
+ * bool mRemoveElement(typeKey aKey)
+ */
+bool cMapTemplate::mRemoveElement(typeKey aKey)
+{
+
+}
+
+/*
+ * void mDrawElements(typeLoop aSize)
+ */
+void cMapTemplate::mDrawElements(typeLoop aSize)
+{
+
+}
+
+
+
+/*
+ * void mPrintAllElements()
+ */
+void cMapTemplate::mPrintAllElements()
+{
+
+}
+
+/********** PUBLIC: END **********/
 
 /* cmaptemplate.cpp */
 /********** END_OF_FILE **********/
