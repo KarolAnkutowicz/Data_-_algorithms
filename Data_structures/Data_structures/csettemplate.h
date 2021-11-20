a7c95a08434e88bc8c13f06e965dcfc6cc11141c
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-11-20 22:42:46 +0100

    Update cSetTemplate
    Add declarations of constructors and other methods in cSetTemplate class.

--- Data_structures/Data_structures/csettemplate.h
+++ Data_structures/Data_structures/csettemplate.h
@@ -7,6 +7,7 @@
 #ifndef CSETTEMPLATE_H
 #define CSETTEMPLATE_H
 
+#include "constantsandtypes.h"
 #include <set>
 
 using namespace std;
@@ -17,8 +18,150 @@ using namespace std;
  */
 class cSetTemplate
 {
+/********** PRIVATE: BEGIN **********/
+
+    /*
+     * set <typeKey, typeData> MapTemplate - zbior
+     * elementow o kluczu typu "typeKey" i wartosciach
+     * typu "typeData".
+     */
+    set <typeKey, typeData> SetTemplate;
+
+    /*
+     * typeData vDrawingRangeValue - pole przechowujace
+     * zakres losowania wartosci elementow.
+     */
+    typeData vDrawingRangeValue;
+
+/********** PRIVATE: END **********/
+
+/********** PUBLIC: BEGIN **********/
+
 public:
+
+    /*
+     * cSetTemplate() - konstruktor wywolywany bez argumentow
+     * PRE:
+     * - brak;
+     * POST:
+     * - utworzenie obiektu klasy cSetTemplate.
+     */
     cSetTemplate();
+
+    /*
+     * cSetTemplate(typeLoop aSize) - konstruktor wywolywany
+     * z jednym argumentem - liczba elementow.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop;
+     * POST:
+     * - utworzenie obiektu klasy cSetTemplate.
+     */
+    cSetTemplate(typeLoop aSize);
+
+    /*
+     * cSetTemplate(typeLoop aSize) - konstruktor wywolywany
+     * z dwoma argumentem - liczba elementow oraz zakresem
+     * losowanie wartosci elementow.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop;
+     * - podanie zakresu losowania elementow (typ: typeData);
+     * POST:
+     * - utworzenie obiektu klasy cMapTemplate.
+     */
+    cSetTemplate(typeLoop aSize, typeData aDrawingRangeValue);
+
+
+
+    /*
+     * typeData getElement(typeKey aKey) - metoda
+     * zwracajaca wartosc wskazanego elementu ze zbioru
+     * wedlug podanego klucza (o ile element istnieje).
+     * PRE:
+     * - podanie klucza (typ: typeKey);
+     * POST:
+     * - zwrocenie elementu ze zbioru (typ: typeData).
+     */
+    typeData getElement(typeKey aKey);
+
+    /*
+     * typeLoop getSetSize() - metoda zwracajaca rozmiar
+     * zbioru.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie rozmiaru zbioru (typ: typeLoop).
+     */
+    typeLoop getSetSize();
+
+    /*
+     * typeLoop getDrawingRangeValue() - metoda zwracajaca
+     * wartosc zakresu losowania elementow.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie wartosci pola vDrawingRangeValue
+     * (typ: typeLoop).
+     */
+    typeLoop getDrawingRangeValue();
+
+    /*
+     * bool getSetIsEmpty() - metoda zawracajaca
+     * informacje o tym czy zbior jest pusty.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy zbior jest pusty
+     * (typ: bool).
+     */
+    bool getSetIsEmpty();
+
+
+
+    /*
+     * void mAddElement(typeKey aKey, typeData aValue) - metoda
+     * dopisujaca element do zbioru.
+     * PRE:
+     * - podanie klucza (typ: aKey);
+     * - podanie wartosci (typ: aValue);
+     * POST:
+     * - brak.
+     */
+    void mAddElement(typeKey aKey, typeData aValue);
+
+    /*
+     * bool mRemoveElement(typeKey aKey) - metoda usuwajaca
+     * wskazany element ze zbioru (o ile element istnieje!).
+     * PRE:
+     * - podanie klucza (typeKey aKey);
+     * POST:
+     * - zwrocenie informacji o tym czy usuniecie elementu
+     * sie udalo (typ: bool).
+     */
+    bool mRemoveElement(typeKey aKey);
+
+    /*
+     * void mDrawElements(typeLoop aSize) - metoda losujaca
+     * elementy zbioru.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop);
+     * POST:
+     * - brak.
+     */
+    void mDrawElements(typeLoop aSize);
+
+
+
+    /*
+     * void mPrintAllElements() - metoda wyswietlajaca
+     * cala zawartosc zbioru (o ile zbior nie jest pusty!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintAllElements();
+
+/********** PUBLIC: END **********/
 };
 
 #endif // CSETTEMPLATE_H
