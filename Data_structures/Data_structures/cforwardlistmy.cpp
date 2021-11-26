7fc5af8e75dc0f5804ab18d03d1a06752a5dcf47
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-11-26 21:47:09 +0100

    Constructors cForwardListMy
    Add definitions of constructors in cForwardListMy class.

--- Data_structures/Data_structures/cforwardlistmy.cpp
+++ Data_structures/Data_structures/cforwardlistmy.cpp
@@ -8,9 +8,131 @@
 
 using namespace std;
 
+/********** PUBLIC: BEGINNING **********/
+
+/*
+ * cForwardListMy() - konstruktor wywolywany bez argumentow.
+ */
 cForwardListMy::cForwardListMy()
 {
+    vSize = 0; // ustanowienie rozmiaru listy jednokierunkowej
+    vDrawingRange = 0; // ustanowienie zakresu losowania elementow
 }
+
+/*
+ * cForwardListMy(typeLoop aSize) - konstruktor wywolywany
+ */
+cForwardListMy::cForwardListMy(typeLoop aSize)
+{
+    vDrawingRange = 0; // ustanowienie zakresu losowania elementow
+    for (typeLoop i = 0; i < aSize; i++) // przejscie po wszystkich elementach
+        mAddElementToEnd(0); // dodanie nowego elementu
+}
+
+/*
+ * cForwardListMy(typeLoop aSize, typeData aDrawingRange) -
+ */
+cForwardListMy::cForwardListMy(typeLoop aSize, typeData aDrawingRange)
+{
+    vDrawingRange = aDrawingRange; // ustanowienie zakresu losowania elementow
+    mDrawElements(aSize); // wywolanie metody losujacej elementy
+}
+
+
+
+/*
+ * typeData getFirstElement() - metoda zwracajaca
+ */
+typeData cForwardListMy::getFirstElement()
+{
+
+}
+
+/*
+ * typeData getLastElement() - metoda zwracajaca
+ */
+typeData cForwardListMy::getLastElement()
+{
+
+}
+
+/*
+ * bool getListIsEmpty() - metoda zawracajaca
+ */
+bool cForwardListMy::getListIsEmpty()
+{
+
+}
+
+
+
+/*
+ * void mAddElementToBegin(typeData aElement) - metoda
+ */
+void cForwardListMy::mAddElementToBegin(typeData aElement)
+{
+
+}
+
+/*
+ * void mAddElementToEnd(typeData aElement) - metoda
+ */
+void cForwardListMy::mAddElementToEnd(typeData aElement)
+{
+
+}
+
+/*
+ * bool mRemoveElementFromBegin() - metoda usuwajaca
+ */
+bool cForwardListMy::mRemoveElementFromBegin()
+{
+
+}
+
+/*
+ * bool mRemoveElementFromEnd() - metoda usuwajaca
+ */
+bool cForwardListMy::mRemoveElementFromEnd()
+{
+
+}
+
+/*
+ * void mDrawElements(typeLoop aSize) - metoda losujaca
+ */
+void cForwardListMy::mDrawElements(typeLoop aSize)
+{
+
+}
+
+
+
+/*
+ * void mPrintFirstElement() - metoda wyswietlajaca
+ */
+void cForwardListMy::mPrintFirstElement()
+{
+
+}
+
+/*
+ * void mPrintLastElement() - metoda wyswietlajaca
+ */
+void cForwardListMy::mPrintLastElement()
+{
+
+}
+
+/*
+ * void mPrintAllElements() - metoda wyswietlajaca
+ */
+void cForwardListMy::mPrintAllElements()
+{
+
+}
+
+/********** PUBLIC: END **********/
 
 /* cforwardlistmy.cpp */
 /********** END_OF_FILE **********/
