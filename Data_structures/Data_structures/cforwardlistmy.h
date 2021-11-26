f274abffb9ea5a87410a38e61eaac70ee19b2ca8
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-11-26 21:21:24 +0100

    Update cForwardListMy
    Add declarations of fields, constructors and other methods in cForwardListMy class.

--- Data_structures/Data_structures/cforwardlistmy.h
+++ Data_structures/Data_structures/cforwardlistmy.h
@@ -7,6 +7,9 @@
 #ifndef CFORWARDLISTMY_H
 #define CFORWARDLISTMY_H
 
+#include "celementsmystructures.h"
+#include <iostream>
+
 using namespace std;
 
 /*
@@ -16,8 +19,219 @@ using namespace std;
  */
 class cForwardListMy
 {
+/********** PRIVATE: BEGINNING **********/
+
+    /*
+     * cElementForwardListAndQueue *vFirst - wskaznik
+     * na pierwszy element listy.
+     */
+    cElementForwardListAndQueue *vFirst;
+
+    /*
+     * cElementForwardListAndQueue *vEnd; - wskaznik
+     * na ostatni element listy.
+     */
+    cElementForwardListAndQueue *vEnd;
+
+    /*
+     * typeLoop vSize - pole przechowujace rozmiar listy.
+     */
+    typeLoop vSize;
+
+    /*
+     * typeData vDrawingRange - pole przechowujace zakres
+     * losowania elementow listy.
+     */
+    typeData vDrawingRange;
+
+/********** PRIVATE: END **********/
+
+/********** PUBLIC: BEGINNING **********/
 public:
+
+    /*
+     * cForwardListMy() - konstruktor wywolywany bez argumentow.
+     * PRE:
+     * - brak;
+     * POST:
+     * - tworzenie obiektu klasy cForwardListMy.
+     */
     cForwardListMy();
+
+    /*
+     * cForwardListMy(typeLoop aSize) - konstruktor wywolywany
+     * z jednym argumentem - liczba elementow jakie mamy
+     * dodac do listy jednokierunkowej.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop);
+     * POST:
+     * - tworzenie obiektu klasy cForwardListMy.
+     */
+    cForwardListMy(typeLoop aSize);
+
+    /*
+     * cForwardListMy(typeLoop aSize, typeData aDrawingRange) -
+     * konstruktor wywolywany z dwoma argumentami - liczba
+     * elementow jakie mamy dodac do listy jednokierunkowej oraz
+     * zakresem z jakiego mamy te elementy losowac.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop);
+     * - podanie maksymalnego zakresu losowania elementow
+     * (typ: typeData);
+     * POST:
+     * - tworzenie obiektu klasy cForwardListMy.
+     */
+    cForwardListMy(typeLoop aSize, typeData aDrawingRange);
+
+
+
+    /*
+     * typeData getFirstElement() - metoda zwracajaca
+     * pierwszy element z listy jednokierunkowej.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie pierwszego elementu z listy
+     * jednokierunkowej (typ: typeData).
+     */
+    typeData getFirstElement();
+
+    /*
+     * typeData getLastElement() - metoda zwracajaca
+     * ostatni element z listy jednokierunkowej.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie ostatniego elementu z listy
+     * jednokierunkowej (typ: typeData).
+     */
+    typeData getLastElement();
+
+    /*
+     * typeLoop getListSize() - zwrocenie rozmiaru listy
+     * jednokierunkowej.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie rozmiaru listy jednokierunkowej
+     * (typ: typeLoop).
+     */
+    typeLoop getListSize();
+
+    /*
+     * typeLoop getDrawingRange() - metoda zwracajaca
+     * wartosc zakresu losowania elementow.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie wartosci pola vDrawingRange
+     * (typ: typeLoop).
+     */
+    typeLoop getDrawingRange();
+
+    /*
+     * bool getListIsEmpty() - metoda zawracajaca
+     * informacje o tym czy lista jednokierunkowa jest pusta.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy lista jednokierunkowa
+     * jest pusta (typ: bool).
+     */
+    bool getListIsEmpty();
+
+
+
+    /*
+     * void mAddElementToBegin(typeData aElement) - metoda
+     * dopisujaca element na sam poczatek listy jednokierunkowej.
+     * PRE:
+     * - podanie wartosci elementu (typ: typeData);
+     * POST:
+     * - brak.
+     */
+    void mAddElementToBegin(typeData aElement);
+
+    /*
+     * void mAddElementToEnd(typeData aElement) - metoda
+     * dopisujaca element na samym koncu listy jednokierunkowej.
+     * PRE:
+     * - podanie wartosci elementu (typ: typeData);
+     * POST:
+     * - brak.
+     */
+    void mAddElementToEnd(typeData aElement);
+
+    /*
+     * bool mRemoveElementFromBegin() - metoda usuwajaca
+     * element z poczatku listy jednokierunkowej (o ile
+     * lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy usuniecie elementu
+     * sie udalo (typ: bool).
+     */
+    bool mRemoveElementFromBegin();
+
+    /*
+     * bool mRemoveElementFromEnd() - metoda usuwajaca
+     * element z konca listy jednokierunkowej (o ile
+     * lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie informacji o tym czy usuniecie elementu
+     * sie udalo (typ: bool).
+     */
+    bool mRemoveElementFromEnd();
+
+    /*
+     * void mDrawElements(typeLoop aSize) - metoda losujaca
+     * elementy listy jednokierunkowej.
+     * PRE:
+     * - podanie liczby elementow (typ: typeLoop);
+     * POST:
+     * - brak.
+     */
+    void mDrawElements(typeLoop aSize);
+
+
+
+    /*
+     * void mPrintFirstElement() - metoda wyswietlajaca
+     * pierwszy element listy jednokierunkowej (o ile
+     * lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintFirstElement();
+
+    /*
+     * void mPrintLastElement() - metoda wyswietlajaca
+     * ostatni element listy jednokierunkowej (o ile
+     * lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintLastElement();
+
+    /*
+     * void mPrintAllElements() - metoda wyswietlajaca
+     * cala zawartosc listy jednokierunkowej (o ile
+     * lista nie jest pusta!).
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintAllElements();
+
+/********** PUBLIC: END **********/
 };
 
 #endif // CFORWARDLISTMY_H
