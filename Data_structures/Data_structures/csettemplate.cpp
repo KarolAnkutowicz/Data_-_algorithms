17e76592f1108b56dcb9c0bd60a1af06d48811c4
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-11-26 11:23:50 +0100

    Update cSetTemplate
    Add definitions of some methods in cSetTemplate class.

--- Data_structures/Data_structures/csettemplate.cpp
+++ Data_structures/Data_structures/csettemplate.cpp
@@ -44,7 +44,15 @@ cSetTemplate::cSetTemplate(typeLoop aSize, typeData aDrawingRangeValue)
  */
 bool cSetTemplate::mRemoveElement(typeData aValue)
 {
-    //
+    for (set<int>::iterator it = SetTemplate.begin(); it != SetTemplate.end(); it++) // przejscie po wszystkich elememntach zbioru
+    {
+        if (*it == aValue) // sprawdzenie czy wartosc jest ta wskazana
+        {
+            SetTemplate.erase(it); // usuniecie elementu
+            return true; // zwrocenie informacji o powodzeniu usuniecia elementu
+        }
+    }
+    return false; // zwrocenie informacji o niepowodzeniu usuniecia elementu
 }
 
 /*
@@ -64,7 +72,8 @@ void cSetTemplate::mDrawElements(typeLoop aSize)
  */
 void cSetTemplate::mPrintAllElements()
 {
-    //
+    for (set<int>::iterator it = SetTemplate.begin(); it != SetTemplate.end(); it++) // przejscie po wszystkich elememntach zbioru
+        cout << *it << endl; // wypisanie elementu
 }
 
 /********** PUBLIC: END **********/
