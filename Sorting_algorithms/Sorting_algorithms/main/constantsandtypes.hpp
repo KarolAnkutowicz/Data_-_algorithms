f5291e747ccdd42433b14a0a8a69ad3fe0bba507
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-02-20 14:00:23 +0100

    Compile in Visual Studio
    Change environment to Visual Studio (previous Qt Creator).

---
+++ Sorting_algorithms/Sorting_algorithms/main/constantsandtypes.hpp
@@ -0,0 +1,34 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: constantsandtypes.hpp
+ */
+
+#ifndef CONSTANTSANDTYPES_HPP
+#define CONSTANTSANDTYPES_HPP
+
+using namespace std;
+
+/*
+ * typedef int typeData - zdefiniowanie synonimu
+ * typu dla rodzaju danych przechowywanych
+ * w obiektach klasy cTable.
+ */
+typedef int typeData;
+/*
+ * typedef unsigned short typeLoop - zdefiniowanie
+ * synonimu typu dla petli i indeksowania.
+ */
+typedef unsigned short typeLoop;
+
+/*
+ * const double constCombGap - stala zdefiniowana
+ * do obliczen odstepow miedzy porownywanymi
+ * wartosciami w algorytmie sortowania grzebieniowego.
+ */
+const double constCombGap = 1.3;
+
+#endif // CONSTANTSANDTYPES_HPP
+
+/* constantsandtypes.hpp */
+/********** END_OF_FILE **********/
---
+++ Sorting_algorithms/Sorting_algorithms/main/ctabhandling.cpp
@@ -0,0 +1,23 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: ctabhandling.cpp
+ */
+
+#include "ctabhandling.hpp"
+
+using namespace std;
+
+/********** PUBLIC: BEGIN **********/
+
+/*
+ * cTabHandling()
+ */
+cTabHandling::cTabHandling()
+{
+}
+
+/********** PUBLIC: END **********/
+
+/* ctabhandling.cpp */
+/********** END_OF_FILE **********/
