b9337328c4de6a4400f78fc6e2566f21d899eefd
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-02-22 23:04:16 +0100

    Refresh folders structure

---
+++ Sorting_algorithms/main/ctabhandling.cpp
@@ -0,0 +1,21 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: ctabhandling.cpp
+ */
+
+#include "ctabhandling.hpp"
+
+/********** PUBLIC: BEGIN **********/
+
+/*
+ * cTabHandling()
+ */
+cTabHandling::cTabHandling()
+{
+}
+
+/********** PUBLIC: END **********/
+
+/* ctabhandling.cpp */
+/********** END_OF_FILE **********/
