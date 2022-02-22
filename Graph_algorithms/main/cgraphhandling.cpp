a9ad6569c62f9921340bacdaca92a5ddf106386b
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-02-22 23:01:12 +0100

    Refresh folders structure

---
+++ Graph_algorithms/main/cgraphhandling.cpp
@@ -0,0 +1,18 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cgraphhandling.cpp
+ */
+
+#include "cgraphhandling.hpp"
+
+/********** PUBLIC: BEGINNING **********/
+
+cGraphHandling::cGraphHandling()
+{
+}
+
+/********** PUBLIC: END **********/
+
+/* cgraphhandling.cpp */
+/********** END_OF_FILE **********/
---
+++ Graph_algorithms/main/main.cpp
@@ -0,0 +1,15 @@
+﻿/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: main.cpp
+ */
+
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
+
+/* main.cpp */
+/********** END_OF_FILE **********/
