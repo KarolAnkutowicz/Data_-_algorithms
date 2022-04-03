141215ad233eb6568b5c71d64dd60e4b23605e7b
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-04-04 00:14:57 +0200

    cUnorderedSetTemplete complete
    Add definitions of all methods in cUnorderedSetTemplate class.

--- Data_structures/main/cunorderedsettemplate.cpp
+++ Data_structures/main/cunorderedsettemplate.cpp
@@ -8,8 +8,61 @@
 
 /********** PUBLIC: BEGINNING **********/
 
+/*
+ * cUnorderedSetTemplate()
+ */
 cUnorderedSetTemplate::cUnorderedSetTemplate()
 {
+    vDrawingRange = 0; // ustanowienie zakresu losowania elementow
+}
+
+/*
+ * cUnorderedSetTemplate(typeLoop aSize, typeData aDrawingRange)
+ */
+cUnorderedSetTemplate::cUnorderedSetTemplate(typeLoop aSize, typeData aDrawingRange)
+{
+    vDrawingRange = aDrawingRange; // ustanowienie zakresu losowania elementow
+    mDrawElements(aSize); // wywolanie metody losujacej elementy
+}
+
+
+
+/*
+ * bool mRemoveElement(typeData aValue)
+ */
+bool cUnorderedSetTemplate::mRemoveElement(typeData aValue)
+{
+    for (std::unordered_set<int>::iterator it = UnorderedSetTemplate.begin(); it != UnorderedSetTemplate.end(); it++) // przejscie po wszystkich elememntach zbioru
+    {
+        if (*it == aValue) // sprawdzenie czy wartosc jest ta wskazana
+        {
+            UnorderedSetTemplate.erase(it); // usuniecie elementu
+            return true; // zwrocenie informacji o powodzeniu usuniecia elementu
+        }
+    }
+    return false; // zwrocenie informacji o niepowodzeniu usuniecia elementu
+}
+
+/*
+ * void mDrawElements(typeLoop aSize)
+ */
+void cUnorderedSetTemplate::mDrawElements(typeLoop aSize)
+{
+    srand(time_t(NULL)); // ustanowienie zmiennej losowej
+    for (typeLoop i = 0; i < aSize; i++) // przejscie po wszystkich elementach
+        mAddElement(rand() % vDrawingRange); // dodanie nowego elementu
+}
+
+
+
+/*
+ * void mPrintAllElements()
+ */
+void cUnorderedSetTemplate::mPrintAllElements()
+{
+    for (std::unordered_set<int>::iterator it = UnorderedSetTemplate.begin(); it != UnorderedSetTemplate.end(); it++) // przejscie po wszystkich elememntach zbioru
+        std::cout << *it << " "; // wypisanie elementu
+    std::cout << "\n"; // przejscie do nowej linii
 }
 
 /********** PUBLIC: END **********/
